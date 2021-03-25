3ca15da8812b1d17df1a84c484974d37d7c70f3d
Ryan Mitchell <rjmitche@usc.edu>
2021-03-24 23:46:49 -0700

    add playground file for swift lesson 1

---
+++ swift/lesson-1.playground/Contents.swift
@@ -0,0 +1,15 @@
+// Welcome to Swift Lesson 1! We'll be using this Playground to check out and practice some Swift basics.
+
+// 1. Variables and Constants: let and var
+
+// 2. Collections: Array, Set, Dictionary
+
+// 3. Control Flow: if, for, for in, while
+
+// 4. Functions
+
+// 5. Optionals
+
+// 6. Classes
+
+// 7. Closures
